49ef66c4ab931e38c6afaabbdc69a19c483835ab
zo-el <joelulahanna@gmail.com>
2025-11-05 13:00:30 -0600

    updated to holochain v0.6.0-dev.33

--- src-tauri/src/generated_arc_factor.rs
+++ src-tauri/src/generated_arc_factor.rs
@@ -1,4 +1,3 @@
 
-        pub const HOLOCHAIN_ARC_FACTOR: &str = "";
-        pub const HOLOCHAIN_VERSION: &str = "0.6.0-dev.32";
-        
+pub const HOLOCHAIN_ARC_FACTOR: &str = "";
+pub const HOLOCHAIN_VERSION: &str = "0.6.0-dev.33";
