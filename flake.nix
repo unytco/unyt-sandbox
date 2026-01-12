dec3495db47bf81aa2ccce449f8d54e5099a7063
Jetttech <ldjetton21@gmail.com>
2026-01-12 10:25:45 -0600

    update: improve build time; restore prev tauri pubkey

--- flake.nix
+++ flake.nix
@@ -39,15 +39,10 @@
           # inputs'.playground.packages.hc-playground
           yarn
           go_1_24
-          fuse
-          zlib
-          inputs.self.packages.${system}.rust
         ]);
 
         shellHook = ''
           export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
-          export APPIMAGE_EXTRACT_AND_RUN=1
-          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc pkgs.zlib pkgs.fuse ]}:$LD_LIBRARY_PATH"
         '';
       };
       devShells.androidDev = pkgs.mkShell {
@@ -60,15 +55,10 @@
           # inputs'.playground.packages.hc-playground
           yarn
           go_1_24
-          fuse
-          zlib
-          inputs.self.packages.${system}.rust
         ]);
 
         shellHook = ''
           export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
-          export APPIMAGE_EXTRACT_AND_RUN=1
-          export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc pkgs.zlib pkgs.fuse ]}:$LD_LIBRARY_PATH"
         '';
       };
     };
