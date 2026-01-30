74d2f0a40c339285f481525ae8c950e757028fec
zo-el <joelulahanna@gmail.com>
2026-01-30 14:55:51 -0600

    v0.50.0-rc.4

--- src-tauri/src/consts.rs
+++ src-tauri/src/consts.rs
@@ -1,5 +1,5 @@
 
-pub const HOLOCHAIN_ARC_FACTOR: &str = "0";
+pub const HOLOCHAIN_ARC_FACTOR: &str = "";
 pub const HOLOCHAIN_VERSION: &str = "0.6.1-rc.0";
 pub const IDENTIFIER_DIR: &str = "co.unyt.unyt.sandbox";
 pub const APP_ID_PREFIX: &str = "unyt-sandbox";
